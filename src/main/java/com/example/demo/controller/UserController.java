f47e951a6babef90da757910939789568c95289c
yiryeong <yiryeong1631@naver.com>
2024-10-28 10:38:25 +0900

    Add UserController to return user ID

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        String userId = "sampleapp1-82240947";
+        return userId;
+    }
+}
